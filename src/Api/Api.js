ee4af689d10813e88359f2aa6c57a6552ed55fa3
Nisheth <nishethteddy129@gmail.com>
2026-04-29 18:09:48 +0530

    updated api url in railway

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "golf-backend-production.up.railway.app"
 });
 
 // Attach JWT token to every request automatically
